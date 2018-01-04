ddfb1a95b578b1a58be39c2961d14c2a298ffb2e
Unknown <tsikerdekis@gmail.com>
2018-01-04 13:44:50 -0800

    Updated example script so that it won't need to read and write to files upon each revision. Minor speed improvement.

--- examples/run_git_persistence.py
+++ examples/run_git_persistence.py
@@ -1,15 +1,16 @@
 # Auxiliary script for running git-persistence on a full repository.
 # This is highly customized and it is meant to used as a demo example for uses of git_persistence.py
+# The script utilizes also git fame for comparisons. If you want to avoid having to use git fame either remove it
+# from the code below or store_each_revision=False
 
-from subprocess import call
 import os
 import csv
 import re
 from git_persistence import GitPersistence
 import parallel_lib
 import sys
-import hashlib
 import datetime
+import subprocess
 
 # Getting the git repo dir from argv
 GIT_PATH = sys.argv[1]
@@ -18,39 +19,37 @@ GIT_PATH = sys.argv[1]
 FILES_TO_EXCLUDE = ["png", "bmp", "dll", "jpg", "jpeg", "exe", "ttf", "ico", "icns", "svg", "ogg"]
 
 
-def execute_and_save(command_list, filename, git_path):
+def execute_and_return(command_list, git_path):
     """ Helper function that runs a command and stores output as a file
 
     :param command_list: list containing command with all parameters
     :type command_list: list
-    :param filename: filename to store output in
-    :type filename: str
     :param git_path: working directory path of git repo
     :type git_path: str
 
-    :return: None
-    :rtype: None
+    :return: output and error (if any)
+    :rtype: tuple
     """
-    f = open(filename, "w")
     os.chdir(git_path)
-    call(command_list, stdout=f)
+    process = subprocess.Popen(command_list,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+    out, err = process.communicate()
     os.chdir("../")
-    f.close()
+    return out, err
 
 
-def read_git_commit_log(filename):
+def read_git_commit_log(data):
     """ Returns commit log from a file containing the exported git log.
     Note: Highly customized for a specific purpose.
 
-    :param filename: filename that contain git log info. Format is expected to be:
+    :param data: specific format list of commits returned by git log:
      --pretty*=format:@%H%n%an%n%ae%n%at%n%cn%n%ce%n%ct@
-    :type filename: str
+    :type data: str
 
     :return: list of formatted entries
     :rtype: list
     """
-    with open(filename, 'r') as file_descriptor:
-        data = file_descriptor.read()
     matches = re.findall('@(.*?)\n(.*?)\n(.*?)\n(.*?)\n(.*?)\n(.*?)\n(.*?)@\n(.*?)(?:$|\n)', data, re.DOTALL)
     return matches
 
@@ -100,12 +99,12 @@ def pre_process():
     :return: files to be processed by script
     :rtype: list
     """
-    execute_and_save(["git", "ls-tree", "--full-tree", "-r", "HEAD"], "files.tmp", GIT_PATH)
+    out, err = execute_and_return(["git", "ls-tree", "--full-tree", "-r", "HEAD"], GIT_PATH)
     files = []
-    with open('files.tmp', 'rt') as csv_file:
-        csv_reader_descriptor = csv.reader(csv_file, delimiter='\t', quotechar=None, escapechar=None)
-        for row in csv_reader_descriptor:
-            files.append(row[1])
+    csv_reader_descriptor = csv.reader(out.decode("utf-8").splitlines(),
+                                       delimiter='\t', quotechar=None, escapechar=None)
+    for row in csv_reader_descriptor:
+        files.append(row[1])
     return files
 
 
@@ -126,26 +125,23 @@ def process_git_file(filename, store_each_revision=True):
         current_file = filename
         print(current_file)
         data_ag = 0
-        hash_name = hashlib.md5(current_file.encode('utf-8')).hexdigest()
-        execute_and_save(["git", "log", "--name-only",
-                          "--pretty=format:@%H%n%an%n%ae%n%at%n%cn%n%ce%n%ct@", "--follow",
-                          current_file],
-                         "commits" + hash_name + ".tmp", GIT_PATH)
+        out, err = execute_and_return(["git", "log", "--name-only",
+                                       "--pretty=format:@%H%n%an%n%ae%n%at%n%cn%n%ce%n%ct@", "--follow",
+                                       current_file], GIT_PATH)
         # Commit list always returned in chronological order by git
-        commit_list = read_git_commit_log("commits" + hash_name + ".tmp")
+        commit_list = read_git_commit_log(out.decode("utf-8"))
         # Storing all revisions for records, this is the same commit log appearing on github
         store_revisions(commit_list, current_file, "commits.tsv")
         i = 0
+        git_fame_processed_commits = []  # auxiliary list so that we won't obtain git fame for the same commit
         for commit in reversed(commit_list):
             aggregate_username = commit[1]
             aggregate_username = aggregate_username.encode("utf-8")
-            execute_and_save(["git", "show", commit[0] + ":" + commit[7]], hash_name, GIT_PATH)
+            out, err = execute_and_return(["git", "show", commit[0] + ":" + commit[7]], GIT_PATH)
             try:
-                with open(hash_name, 'r', encoding="utf-8") as my_file:
-                    data = my_file.read()
+                data = out.decode("utf-8")
             except UnicodeEncodeError:  # Try alternative encoding just in case otherwise it is a binary file probably
-                with open(hash_name, 'r', encoding="utf-16") as my_file:
-                    data = my_file.read()
+                data = out.decode("utf-16")
             data_ag += len(data.splitlines(False))
 
             # Start new tracking or update existing (depending on whether we look at the same file)
@@ -172,24 +168,22 @@ def process_git_file(filename, store_each_revision=True):
                 # Get additional git-fame (blame) stats per revision (there is a limitation on the basis of date)
                 # git-fame uses always the latest commit for that date
                 # The solution below produces duplicates that need to be removed during the analysis (fairly easy)
-                git_fame_file_name = "gitfame"+hash_name
-                execute_and_save(["git", "fame", "--format=csv", "-h", "--before",
-                                  datetime.datetime.fromtimestamp(int(commit[3])).strftime('%Y-%m-%d')],
-                                 git_fame_file_name, GIT_PATH)
-                f = open("git_fame_per_rev.tsv", "a")
-                with open(git_fame_file_name, 'r', encoding="utf-8") as git_fame_file:
-                    git_fame_lines = git_fame_file.readlines()
-                os.remove(git_fame_file_name)
-                a = 0
-                for line in git_fame_lines:
-                    if a != 0:  # skip first line
-                        f.write("%s,%s\n" % (line.strip(), commit[0]))
-                    a += 1
-                f.close()
+                if commit[0] not in git_fame_processed_commits:
+                    out, err = execute_and_return(["git", "fame", "--format=csv", "-h", "--before",
+                                                   datetime.datetime.fromtimestamp(int(commit[3])).
+                                                  strftime('%Y-%m-%d')],
+                                                  GIT_PATH)
+                    f = open("git_fame_per_rev.tsv", "a")
+                    git_fame_lines = out.decode("utf-8").splitlines()
+                    a = 0
+                    for line in git_fame_lines:
+                        if a != 0:  # skip first line
+                            f.write("%s,%s\n" % (line.strip(), commit[0]))
+                        a += 1
+                    f.close()
+                    git_fame_processed_commits.append(commit[0])
 
             i += 1
-            os.remove(hash_name)
-        os.remove("commits" + hash_name + ".tmp")
         results = tracking.calculate_ownership()
         f = open("persistence_scores.tsv", "a")
 
@@ -223,6 +217,6 @@ def process_git_file(filename, store_each_revision=True):
 # As it stands the script below utilizes 1 core (value can be changed)
 # and applies PCC to scientist repository    
 if __name__ == '__main__':
-    files = pre_process()
+    FILES = pre_process()
     reset_files()
-    parallel_lib.parallel_process(8, files, process_git_file)
+    parallel_lib.parallel_process(8, FILES, process_git_file)
